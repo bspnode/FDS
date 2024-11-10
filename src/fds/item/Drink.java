eb1997de501082c4279aa0a82f65e3db12679b70
bspnode <buckledown808@gmail.com>
2024-11-09 21:15:17 -0800

    Added New Source

---
+++ src/fds/item/Drink.java
@@ -0,0 +1,21 @@
+﻿package fds.item;
+
+//==============================================//
+// File: Drink.java                             //
+// Author: Austin Tavares                       //
+// Date: 11/09/2024                             //
+// Purpose: Drink Products (Soda, Water, etc.)  //
+//==============================================//
+
+public class Drink extends Food
+{
+    public Drink(String name, double price, double calories, double weight)
+    {
+        super(name, price, calories, weight);
+    }
+
+    public Drink(Food food)
+    {
+        super(food);
+    }
+}
